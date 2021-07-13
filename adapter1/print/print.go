e4ed37f7b0e719c9776e786154abe62ebac7c9e0
nky163 <nky8888163@gmail.com>
2021-07-13 13:34:47 +0000

    Add Comment

--- adapter1/print/print.go
+++ adapter1/print/print.go
@@ -2,16 +2,20 @@ package print
 
 import "adapter1/banner"
 
+// Target
 type Printer interface {
 	PrintWeak()
 	PrintStrong()
 }
 
+// Adapter
 type PrintBanner struct {
+	// Adaptee
 	*banner.Banner // 継承的な...
 }
 
 // Printerインターフェースを返すようにしてみた...
+// 使う側はインターフェースに依存する
 func NewPrinter(string_ string) Printer {
 	p := &PrintBanner{banner.NewBanner(string_)}
 	return p
